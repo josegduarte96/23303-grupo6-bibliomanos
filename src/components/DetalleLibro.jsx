528ec053099fd2b169d000fda1bbf50492f52346
Jose Duarte <josegduarte96@gmail.com>
2023-06-07 12:51:17 -0300

    merge all branch

---
+++ src/components/DetalleLibro.jsx
@@ -0,0 +1,33 @@
+// Detalle de libro, imagen, descripcion, boton de favoritos 
+// import {Link} from 'react-router-dom';
+import {Card, Row, Col, Button} from 'react-bootstrap';
+
+
+function DetalleLibro() {
+
+  return (
+    <div className="card mx-auto mt-3 shadow" style={{ maxWidth: '80vw' }}>
+      <Row className="g-0">
+        <Col md={4}>
+          <Card.Img src="https://covers.openlibrary.org/b/id/12547191-L.jpg" className="img-fluid mx-auto" alt="portada del libro" />
+        </Col>
+        <Col md={8}>
+          <div className="card-body">
+            <Card.Title>TITULO DEL LIBRO</Card.Title>
+                   <Card.Subtitle className="mb-2 text-body-secondary">Autor del libro</Card.Subtitle>
+            <Card.Text> RESUMEN DEL LIBRO Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolore quidem non neque veniam vel perferendis minima facilis. Vel ratione suscipit dicta voluptate nemo molestias quibusdam inventore quo natus eligendi? </Card.Text>
+            <div class="d-grid gap-2 d-md-block mt-5">
+              
+            <Button href="#" className="btn mx-2"> Leer</Button>
+            
+            <Button href="#" className="btn mx-2">Favoritos</Button>
+            </div>
+          </div>
+        </Col>
+      </Row>
+          {/* <Link to="/DetalleLibro"></Link>  */}
+    </div>
+  );
+};
+
+export default DetalleLibro;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,48 @@
-import { Link } from "react-router-dom"
+import React, {useState} from 'react'
 
-function Login() {
-  return (
-    <>
-      <div>
-        <p className="h1">Login Page</p>
-        {<Link to={"/"}>Volver a inicio..</Link>}
-      </div>
-    </>
+const Login = () => {
+
+const [usuario, setUsuario] = useState(false)
+
+return (
+<div>
+
+{/* Creo el formulario de identificacion */}
+
+<h1>{usuario ? 'registrate': 'inicia sesion'}</h1>
+<form>
+    <div className='mb-3'>
+      <label className='form-label'>Usuario o correo electronico </label>
+      <input type="email" className='form-control' placeholder='Ingresar email' id='email' />
+    </div>
+
+    <div className='mb-3'>
+      <label className='form-label'>Contraseña</label>
+      <input type="email" className='form-control' placeholder='Ingresar Contraseña' id='password' />
+    </div>
+
+    <button className='btn btn-primary' type='submit'>
+      {usuario ? 'registrate' : 'inicia sesion'}
+    </button>
+</form>
+
+<div className='form-group'>
+    <button className='btn btn-secondary mt-4 form-control'onclick={()=>setUsuario(!usuario)}>
+      {usuario ? 'ya tienes una cuenta? Inicia Sesion':  'Olvidaste tu contraseña'}
+    </button>
+    <button className='btn btn-secondary mt-4 form-control'onclick={()=>setUsuario(!usuario)}>
+      {usuario ? 'ya tienes una cuenta? Inicia Sesion':  'Quiero Registrarme'}
+    </button>
+</div>
+
+</div>
+
+
+
   )
 }
 
+
 export default Login
+
+
