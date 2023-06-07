f49d9691bf626453105771591e19583bce0fa3be
JAC <jacaffo@gmail.com>
2023-06-07 11:41:37 -0300

    Creacion Componente Login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 //import { Router } from 'react-router-dom';
+import React, {useState} from 'react';
 import './App.css';
 //import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './componentes/Login';
 import LibreriaOnline from './componentes/LibroGal2';
 //import Info from './componentes/Info';
 //import "./componentes/styles.css";
 
-
-
  
 function App() {
+
+  
+
   return (
     <div className="App">
-      
+
+      <Login/>
       <LibreriaOnline/>
     </div>
 
--- src/componentes/LibroGal2.jsx
+++ src/componentes/LibroGal2.jsx
@@ -10,6 +10,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "./styles.css";
 
 const LibreriaOnline = () => {
+
   const [romanceBooks, setRomanceBooks] = useState([]);
   const [childrenBooks, setChildrenBooks] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
