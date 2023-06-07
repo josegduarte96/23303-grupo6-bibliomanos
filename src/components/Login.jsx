e1b54220323dfbe0a15764a6213e8f32061a000d
Jose Duarte <josegduarte96@gmail.com>
2023-06-07 13:01:21 -0300

    fix bug: error en la importacion de react

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,48 +1,39 @@
-import React, {useState} from 'react'
+import { useState } from "react"
 
 const Login = () => {
+  const [usuario, setUsuario] = useState(false)
 
-const [usuario, setUsuario] = useState(false)
+  return (
+    <div>
+      {/* Creo el formulario de identificacion */}
 
-return (
-<div>
+      <h1>{usuario ? "registrate" : "inicia sesion"}</h1>
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Usuario o correo electronico </label>
+          <input type="email" className="form-control" placeholder="Ingresar email" id="email" />
+        </div>
 
-{/* Creo el formulario de identificacion */}
+        <div className="mb-3">
+          <label className="form-label">Contraseña</label>
+          <input type="email" className="form-control" placeholder="Ingresar Contraseña" id="password" />
+        </div>
 
-<h1>{usuario ? 'registrate': 'inicia sesion'}</h1>
-<form>
-    <div className='mb-3'>
-      <label className='form-label'>Usuario o correo electronico </label>
-      <input type="email" className='form-control' placeholder='Ingresar email' id='email' />
+        <button className="btn btn-primary" type="submit">
+          {usuario ? "registrate" : "inicia sesion"}
+        </button>
+      </form>
+
+      <div className="form-group">
+        <button className="btn btn-secondary mt-4 form-control" onClick={() => setUsuario(!usuario)}>
+          {usuario ? "ya tienes una cuenta? Inicia Sesion" : "Olvidaste tu contraseña"}
+        </button>
+        <button className="btn btn-secondary mt-4 form-control" onClick={() => setUsuario(!usuario)}>
+          {usuario ? "ya tienes una cuenta? Inicia Sesion" : "Quiero Registrarme"}
+        </button>
+      </div>
     </div>
-
-    <div className='mb-3'>
-      <label className='form-label'>Contraseña</label>
-      <input type="email" className='form-control' placeholder='Ingresar Contraseña' id='password' />
-    </div>
-
-    <button className='btn btn-primary' type='submit'>
-      {usuario ? 'registrate' : 'inicia sesion'}
-    </button>
-</form>
-
-<div className='form-group'>
-    <button className='btn btn-secondary mt-4 form-control'onclick={()=>setUsuario(!usuario)}>
-      {usuario ? 'ya tienes una cuenta? Inicia Sesion':  'Olvidaste tu contraseña'}
-    </button>
-    <button className='btn btn-secondary mt-4 form-control'onclick={()=>setUsuario(!usuario)}>
-      {usuario ? 'ya tienes una cuenta? Inicia Sesion':  'Quiero Registrarme'}
-    </button>
-</div>
-
-</div>
-
-
-
   )
 }
 
-
 export default Login
-
-
