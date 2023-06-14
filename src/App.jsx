a30e711c9bd818cc13e6fecab9a29907c1f72aa5
MMatildeR <mmatirod@gmail.com>
2023-06-14 10:57:03 -0300

    Recolección de correos en base de datos

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,27 @@ import Footer from "./components/Footer"
 import LibreriaOnline from "./components/LibroGal2"
 import MainLayout from "./components/MainLayout"
 import Navbar from "./components/Navbar"
+import React, {useState} from 'react'
+import Login from "./components/Login"
+
+import firebaseApp from './usuariosBD'
+import {getAuth, onAuthStateChanged} from 'firebase/auth'
+const auth = getAuth(firebaseApp)
+
 
 function App() {
+
+  const [usuario, setUsuario] = useState(null)
+
+  onAuthStateChanged(auth, (usuarioEnBd) =>{ 
+    if(usuarioEnBd){
+      setUsuario(usuarioEnBd)
+    }
+    else {
+      setUsuario(null)
+    }
+  })
+
   return (
     <>
       <div style={{ maxWidth: "100vw", overflow: "hidden" }}>
@@ -11,8 +30,10 @@ function App() {
         <section className="py-3 d-flex justify-content-center align-items-center main-content">
           <h1 className="text-center">¿Buscando tu próxima lectura?</h1>
         </section>
-        <div className="container-fluid overflow-hidden">
-          <MainLayout />
+        <div className="container- fluid overflow-hidden">
+          <div>
+            {usuario ? <MainLayout correoUsuario = {/*usuario ?*/ usuario.email /*: null*/}/> : <Login/>}
+          </div>
           <LibreriaOnline />
         </div>
         <Footer />
