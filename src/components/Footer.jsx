cb4f6a7aaa989ce7848b0f1900a273a2d95ebf80
JAC <jacaffo@gmail.com>
2023-07-06 10:22:02 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
           <div className="col-md-12 text-center">
             <p>Grupo #6, Comisión 23303. Codo A Codo (Agencia de Aprendizaje a lo largo de vida)</p>
             <p> Integrantes: </p>
-            <p> -Jose Duarte <a href=""> (a Designar) </a>-Fabiana Lera <a href=""> (a Designar) </a>-Matilde <a href=""> (a Designar) </a>-Leonardo Ponce <a href="https://www.linkedin.com/in/leonardo-ponce/"> (Website) </a>-Eliana Moguilevsky <a href="https://www.linkedin.com/in/elianamoguilevsky/"> (Website) </a>-Jose Caffo <a href="https://www.linkedin.com/in/jacaffo/"> (Website) </a></p>
+            <p> -Jose Duarte <a href=""> (a Designar) </a>-Fabiola Lera <a href="https://www.linkedin.com/in/fabyolalera"> (Website) </a>-Matilde <a href=""> (a Designar) </a>-Leonardo Ponce <a href="https://www.linkedin.com/in/leonardo-ponce/"> (Website) </a>-Eliana Moguilevsky <a href="https://www.linkedin.com/in/elianamoguilevsky/"> (Website) </a>-Jose Caffo <a href="https://www.linkedin.com/in/jacaffo/"> (Website) </a></p>
           </div>
         </div>
       </div>
